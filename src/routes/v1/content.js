7b6a23f4174abb353aa426c1801a3b1c745c48a6
KristinaBe <ipasauliokrasta@gmail.com>
2021-11-30 22:23:46 +0200

    added isWinn key to the response

--- src/routes/v1/content.js
+++ src/routes/v1/content.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-alert */
 const express = require('express');
 const mysql = require('mysql2/promise');
 // const Joi = require('joi');
@@ -15,52 +17,14 @@ router.get('/dice/', async (req, res) => {
       Math.floor(Math.random() * 6) + 1,
       Math.floor(Math.random() * 6) + 1,
     ];
-    return res.status(200).send({ numbers });
+
+    isWinner = numbers.every((val, i, arr) => val === arr[0]);
+
+    return res.status(200).send({ isWinner, numbers });
   } catch (err) {
     return res
       .status(500)
       .send({ err: 'Issue by rolling dice. Try again', e: err });
-  }
-});
-
-// GET all groups
-router.get('/groups/', isLoggedIn, async (req, res) => {
-  try {
-    const con = await mysql.createConnection(dbConfig);
-
-    const [data] = await con.execute(`
-        SELECT * FROM cao_groups
-    `);
-    await con.end();
-    return res.send(data);
-  } catch (err) {
-    return res
-      .status(500)
-      .send({ err: 'Issue by getting list of groups. Try again' });
-  }
-});
-
-// ADD a group to the database
-router.post('/groups/', isLoggedIn, async (req, res) => {
-  const groupDetails = req.body;
-
-  try {
-    const con = await mysql.createConnection(dbConfig);
-
-    await con.execute(`
-        INSERT INTO cao_groups (name)
-        VALUES (${mysql.escape(groupDetails.id)})
-    `);
-
-    await con.end();
-
-    return res.status(200).send({
-      msg: `Group ${groupDetails.id} created. Please refresh your page.`,
-    });
-  } catch (err) {
-    return res.status(500).send({
-      err: 'Issue by creating a group. Group name is expected to be an unique number. Try again.',
-    });
   }
 });
 
